6d84bd26b4f3b7cfa7da281764279f6b5b2b8134
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2022-12-03 10:14:36 +0530

    Create 12 Oct 2022.java

---
+++ 12 Oct 2022.java
@@ -0,0 +1,12 @@
+// Problem Statement : 12 Oct 2022
+
+// Accept two list from the user and find common element from the both list
+
+// Input: L1={10,20,30,40}, L2={40,50,60,70}
+// Output : 40
+
+// Input : L1={'A','B','C'}, L2={'D','B','A','E'}
+// Output : A, B
+
+
+// Solution:-
