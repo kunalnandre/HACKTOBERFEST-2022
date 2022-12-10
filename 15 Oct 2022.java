2dc4cf70fcc0a6fef72bf3ee2cb8aa38bca7a2cd
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2022-12-10 21:15:16 +0530

    Update 15 Oct 2022.java

--- 15 Oct 2022.java
+++ 15 Oct 2022.java
@@ -1,1 +1,9 @@
+// Problem Statement : 15 Oct 2022
 
+// Accept array from user and rotate it from left by d place:
+
+// Input: arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
+// Output: 3 4 5 6 7 1 2
+
+// Input: arr[] = {3, 4, 5, 6, 7, 1, 2}, d=3
+// Output: 6 7 1 2 3 4 5
